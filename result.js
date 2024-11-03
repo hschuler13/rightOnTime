84a869a42ec85d4c8ad9ccc906146991baada867
KhalilE792 <khalilecollege@gmail.com>
2024-11-03 11:01:05 -0600

    country images

--- result.js
+++ result.js
@@ -1,5 +1,18 @@
 countriesList = ["Brazil!", "Canada!", "Egypt!", "Greece!", "Indonesia!", "Italy!", "Lebanon!", "Mexico!", "Nigeria!", "Vietnam!"]
 
+countryImages = [
+    "https://mapsvg.com/maps/brazil",
+"https://mapsvg.com/maps/canada",
+"https://mapsvg.com/maps/egypt",
+"https://mapsvg.com/maps/greece",
+"https://mapsvg.com/maps/indonesia",
+"https://mapsvg.com/maps/italy",
+"https://simplemaps.com/svg/country/lb",
+"https://mapsvg.com/maps/mexico",
+"https://mapsvg.com/maps/nigeria",
+"https://mapsvg.com/maps/vietnam"
+]
+
 function randomNumber(max){
     return Math.floor(Math.random() * max) 
 }
