ef529eac722e135d4db46d91773fd6477932eeb3
hschuler13 <hschul04@gmail.com>
2024-11-03 11:16:31 -0600

    added map images

--- result.js
+++ result.js
@@ -1,9 +1,21 @@
 countriesList = ["Brazil!", "Canada!", "Egypt!", "Greece!", "Indonesia!", "Italy!", "Lebanon!", "Mexico!", "Nigeria!", "Vietnam!"]
-
+countryImages = ["brazil.svg",
+    "canada.svg",
+    "egypt.svg",
+    "greece.svg",
+    "indonesia.svg",
+    "italy.svg",
+    "lb.svg",
+    "mexico.svg",
+    "nigeria.svg",
+    "vietnam.svg"]
+    
 function randomNumber(max){
     return Math.floor(Math.random() * max) 
 }
 
 function countrySelect(){
-    document.getElementsByTagName('p')[1].innerText = countriesList[randomNumber(10)];
+    let number = randomNumber(10)
+    document.getElementsByTagName('p')[1].innerText = countriesList[number]
+    document.getElementsByTagName('img')[0].src = countryImages[number]
 }
